6806ab917c06bd00265fb253a13196cd350d8909
Corin Langosch <info@netskin.com>
2010-09-17 17:58:23 +0100

    only check uniqueness if the column changed

--- lib/sequel_sexy_validations/validators/uniqueness.rb
+++ lib/sequel_sexy_validations/validators/uniqueness.rb
@@ -7,6 +7,8 @@ module Sequel
           def self.validate(model, attribute, value, options)
             return unless value
 
+            return unless model.changed_columns.include?(attribute)
+
             unless model.class.filter(~:id => model.id, attribute => value).empty?
               model.errors.add(attribute, "bereits vergeben")
             end
